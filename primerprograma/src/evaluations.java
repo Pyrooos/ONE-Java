3559de760c631eaac8481fb8a12bb009a4eac28d
pyrooos <cristian_guher@outlook.com>
2024-04-08 20:46:41 -0600

    add new code named evaluations where try to explain loops and conditionals

---
+++ primerprograma/src/evaluations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class evaluations {
+    public static void main(String[] args) {
+
+        //simple example how to make a loop with a conditionals While, if and show how to limit the above writing '-1'
+
+        Scanner keyboard = new Scanner(System.in);
+        double rating = 0;
+        double averageOfEvaluations=0;
+        double totalRatings=0;
+
+        while (rating !=-1) {
+            System.out.println("Write the rating would give it ");
+            rating = keyboard.nextDouble();
+
+            if (rating != -1) {
+                averageOfEvaluations += rating;
+                totalRatings++;
+
+            }
+        }System.out.println("The average rating of the movie is:" + averageOfEvaluations / totalRatings);
+    }
+}
