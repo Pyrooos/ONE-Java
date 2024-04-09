e479df33ca267364a003171a38654bb30eaba5c5
pyrooos <cristian_guher@outlook.com>
2024-04-08 20:00:33 -0600

    the codes are update and up new code named reading

--- primerprograma/src/Reading.java
+++ primerprograma/src/Reading.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Reading {
-    
+    public static void main(String[] args) {
+        //This exercise is a way to display the final form of the program, the frontend still needs to be finished.
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("write the name of your favorite movie");
+        String movie= keyboard.nextLine();
+        System.out.println("Now write the release date");
+        int releaseDate = keyboard.nextInt();
+        System.out.println("Finally, what rating would you give it?");
+        double rating = keyboard.nextDouble();
+
+        System.out.println("Movie = " + movie);
+        System.out.println("ReleaseDate = " + releaseDate);
+        System.out.println("Rating = " + rating);
+
+    }
 }
